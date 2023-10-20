f363453ea09f109d5593968740ded1c985aedef3
Krasimir Topchiyski <kitopchiyski@gmail.com>
2023-10-20 22:16:32 +0300

    Add utility method getReadableDateTime(long)

--- app/src/main/java/org/metatrans/commons/TimeUtils.java
+++ app/src/main/java/org/metatrans/commons/TimeUtils.java
@@ -1,6 +1,11 @@
 package org.metatrans.commons;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class TimeUtils {
 	
 	public static String time2string(long time) {
@@ -24,5 +29,26 @@ public class TimeUtils {
 		
 		return time_string;
 	}
-	
+
+
+	public static String getReadableDateTime(long timestamp) {
+
+		try{
+
+			Calendar calendar = Calendar.getInstance();
+			TimeZone tz = TimeZone.getDefault();
+			calendar.setTimeInMillis(timestamp);
+			//calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Date currenTimeZone = (Date) calendar.getTime();
+
+			return sdf.format(currenTimeZone);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return "";
+	}
 }
